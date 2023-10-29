e91e9d3b63b39052be4befb63c5be92b40d50d16
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2023-10-29 22:37:46 +0530

    login and register endpoints and JWT authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import asyncHandler from "express-async-handler";
 
 import User from "../models/userModel.js";
+import generateToken from "../utils/createToken.js";
 
 // @desc    Register new user
 // @route   POST /api/v1/users
@@ -24,10 +25,15 @@ const createUser = asyncHandler(async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   const hashedPass = await bcrypt.hash(password, salt);
 
+  //create a new user
   const newUser = new User({ username, email, password: hashedPass });
 
   try {
     await newUser.save();
+
+    //To generate jwt token
+    generateToken(res, newUser._id);
+
     res.json({
       _id: newUser._id,
       username: newUser.username,
@@ -40,4 +46,30 @@ const createUser = asyncHandler(async (req, res) => {
 
   //   res.json({ message: "user created successfully", username, password, email });
 });
-export { createUser };
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  //verify user exists
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (isPasswordValid) {
+      generateToken(res, existingUser._id);
+      res.status(200).json({
+        _id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+        isAdmin: existingUser.isAdmin,
+      });
+      return;
+    }
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
+});
+export { createUser, loginUser };
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser, loginUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/", createUser);
+router.post("/auth", loginUser);
 
 export default router;

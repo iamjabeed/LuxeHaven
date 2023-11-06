93266558e1ff74f7d56284f03f9fc3f8e877f097
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2023-11-06 14:10:48 +0530

    Product API slice

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,3 +1,5 @@
 export const BASE_URL = "";
 export const USERS_URL = "/api/v1/users";
 export const CATEGORY_URL = "/api/v1/category";
+export const PRODUCT_URL = "/api/v1/products";
+export const UPLOAD_URL = "/api/v1/upload";

451c68b9cc563ed76c96e78daf3b03c620ffcf2f
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2023-11-06 12:23:16 +0530

    File handling with multer

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 // packages
-
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -10,6 +10,7 @@ import connectDB from "./config/db.js";
 import userRouter from "./routes/userRouter.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -28,6 +29,10 @@ app.use(cookieParser());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 console.clear();
 

b788ac4ed073ca54b95271a00c1868e5ec91f0e2
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2023-11-21 22:21:06 +0530

    HeroBottom component

--- frontend/src/components/HeroBottom.jsx
+++ frontend/src/components/HeroBottom.jsx
@@ -2,9 +2,22 @@ import ContentWrapper from "./ContentWrapper";
 
 const HeroBottom = () => {
   return (
-    <div className="h-[100vh] w-full bg-[#FF4800]">
+    <div className="lg:min-h-[100vh] w-full bg-[#FF4800] text-[#000000] p-4">
       <ContentWrapper>
-        <div>Hello</div>
+        <div className="">
+          <h4 className="font-canope text-left uppercase mt-0 mb-0 font-canopee text-[3rem] lg:text-[10rem] leading-[60px] lg:leading-[170px] font-bold">
+            ELEVATING
+          </h4>
+          <h4 className="font-canope text-left uppercase mt-0 mb-0 font-canopee text-[3rem] lg:text-[10rem] leading-[60px] lg:leading-[170px] font-bold">
+            EXPERIENCES &
+          </h4>
+          <h4 className="font-canope text-left uppercase mt-0 mb-0 font-canopee text-[3rem] lg:text-[10rem] leading-[60px] lg:leading-[170px] font-bold">
+            CRAFTING REALITIE
+          </h4>
+          {/* <h4 className="font-canope text-left uppercase mt-0 mb-0 font-canopee text-[3rem] lg:text-[10rem] leading-[60px] lg:leading-[170px] font-bold">
+            REALITIE
+          </h4> */}
+        </div>
       </ContentWrapper>
     </div>
   );

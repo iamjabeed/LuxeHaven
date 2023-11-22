22555f2a47c5d33ea4468316fc32a7f77e6834b2
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2023-11-22 16:41:32 +0530

    Shipping page

--- frontend/src/components/ProgressSteps.jsx
+++ frontend/src/components/ProgressSteps.jsx
@@ -1,14 +1,16 @@
 const ProgressSteps = ({ step1, step2, step3 }) => {
   return (
-    <div className="flex justify-center items-center space-x-4">
+    <div className="flex justify-center items-center ">
       <div className={`${step1 ? "text-green-500" : "text-gray-300"}`}>
-        <span className="ml-2">Login</span>
+        <span className="">Login</span>
         <div className="mt-2 text-lg text-center">✅</div>
       </div>
 
       {step2 && (
         <>
-          {step1 && <div className="h-0.5 w-[10rem] bg-green-500"></div>}
+          {step1 && (
+            <div className="h-0.5 w-[4rem] lg:w-[10rem] bg-green-500"></div>
+          )}
           <div className={`${step1 ? "text-green-500" : "text-gray-300"}`}>
             <span>Shipping</span>
             <div className="mt-2 text-lg text-center">✅</div>
@@ -18,13 +20,15 @@ const ProgressSteps = ({ step1, step2, step3 }) => {
 
       <>
         {step1 && step2 && step3 ? (
-          <div className="h-0.5 w-[10rem] bg-green-500"></div>
+          <div className="h-0.5 w-[4rem] lg:w-[10rem] bg-green-500"></div>
         ) : (
           ""
         )}
 
         <div className={`${step3 ? "text-green-500" : "text-gray-300"}`}>
-          <span className={`${!step3 ? "ml-[10rem]" : ""}`}>Summary</span>
+          <span className={`${!step3 ? "ml-[2rem] lg:ml-[10rem]" : ""}`}>
+            Summary
+          </span>
           {step1 && step2 && step3 ? (
             <div className="mt-2 text-lg text-center">✅</div>
           ) : (
--- frontend/src/pages/Orders/Shipping.jsx
+++ frontend/src/pages/Orders/Shipping.jsx
@@ -6,6 +6,7 @@ import {
   savePaymentMethod,
 } from "../../redux/features/cart/cartSlice";
 import ProgressSteps from "../../components/ProgressSteps";
+import ContentWrapper from "../../components/ContentWrapper";
 
 const Shipping = () => {
   const cart = useSelector((state) => state.cart);
@@ -38,81 +39,91 @@ const Shipping = () => {
   }, [navigate, shippingAddress]);
 
   return (
-    <div className="container mx-auto mt-10">
-      <ProgressSteps step1 step2 />
-      <div className="mt-[2rem] flex justify-around items-center flex-wrap">
-        <form onSubmit={submitHandler} className="w-[40rem]">
-          <h1 className="text-2xl font-semibold mb-4">Shipping</h1>
-          <div className="mb-4">
-            <label className="block text-white mb-2">Address</label>
-            <input
-              type="text"
-              className="w-full p-2 border rounded"
-              placeholder="Enter address"
-              value={address}
-              required
-              onChange={(e) => setAddress(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-white mb-2">City</label>
-            <input
-              type="text"
-              className="w-full p-2 border rounded"
-              placeholder="Enter city"
-              value={city}
-              required
-              onChange={(e) => setCity(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-white mb-2">Postal Code</label>
-            <input
-              type="text"
-              className="w-full p-2 border rounded"
-              placeholder="Enter postal code"
-              value={postalCode}
-              required
-              onChange={(e) => setPostalCode(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-white mb-2">Country</label>
-            <input
-              type="text"
-              className="w-full p-2 border rounded"
-              placeholder="Enter country"
-              value={country}
-              required
-              onChange={(e) => setCountry(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-400">Select Method</label>
-            <div className="mt-2">
-              <label className="inline-flex items-center">
-                <input
-                  type="radio"
-                  className="form-radio text-pink-500"
-                  name="paymentMethod"
-                  value="PayPal"
-                  checked={paymentMethod === "PayPal"}
-                  onChange={(e) => setPaymentMethod(e.target.value)}
-                />
-
-                <span className="ml-2">PayPal or Credit Card</span>
-              </label>
-            </div>
-          </div>
-
-          <button
-            className="bg-pink-500 text-white py-2 px-4 rounded-full text-lg w-full"
-            type="submit"
+    <div className="bg-[#0E1629] mx-auto mt-10 min-h-screen">
+      <ContentWrapper>
+        <ProgressSteps step1 step2 />
+        <div className="mt-[2rem] flex w-full h-full flex-wrap">
+          <form
+            onSubmit={submitHandler}
+            className="w-[40rem] mx-auto ml-[13%] md:ml-auto"
           >
-            Continue
-          </button>
-        </form>
-      </div>
+            <h1 className="text-2xl font-semibold mb-4">Shipping</h1>
+            <div className="mb-4">
+              <label className="block text-white mb-2">Address</label>
+              <input
+                type="text"
+                className=" mt-1 p-2 border rounded  w-[320px] md:w-[460px] 2xl:w-[520px] bg-[#0E1629] placeholder-[#eaeaeab9] text-[#F6F6F6] outline-none border-[#57575b] focus:border-[#FF2E63]"
+                placeholder="Enter address"
+                value={address}
+                required
+                onChange={(e) => setAddress(e.target.value)}
+                autoComplete="off"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-white mb-2">City</label>
+              <input
+                type="text"
+                className="t-1 p-2 border rounded  w-[320px] md:w-[460px] 2xl:w-[520px] bg-[#0E1629] placeholder-[#eaeaeab9] text-[#F6F6F6] outline-none border-[#57575b] focus:border-[#FF2E63]"
+                placeholder="Enter city"
+                value={city}
+                required
+                onChange={(e) => setCity(e.target.value)}
+                autoComplete="off"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-white mb-2">Postal Code</label>
+              <input
+                type="text"
+                className="t-1 p-2 border rounded  w-[320px] md:w-[460px] 2xl:w-[520px] bg-[#0E1629] placeholder-[#eaeaeab9] text-[#F6F6F6] outline-none border-[#57575b] focus:border-[#FF2E63]"
+                placeholder="Enter postal code"
+                value={postalCode}
+                required
+                onChange={(e) => setPostalCode(e.target.value)}
+                autoComplete="off"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-white mb-2">Country</label>
+              <input
+                type="text"
+                className="t-1 p-2 border rounded  w-[320px] md:w-[460px] 2xl:w-[520px] bg-[#0E1629] placeholder-[#eaeaeab9] text-[#F6F6F6] outline-none border-[#57575b] focus:border-[#FF2E63]"
+                placeholder="Enter country"
+                value={country}
+                required
+                onChange={(e) => setCountry(e.target.value)}
+                autoComplete="off"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block">Select Method</label>
+              <div className="mt-2">
+                <label className="inline-flex items-center">
+                  <input
+                    type="radio"
+                    className="form-radio text-pink-500"
+                    name="paymentMethod"
+                    value="PayPal"
+                    checked={paymentMethod === "PayPal"}
+                    onChange={(e) => setPaymentMethod(e.target.value)}
+                    autoComplete="off"
+                  />
+
+                  <span className="ml-2">PayPal or Credit Card</span>
+                </label>
+              </div>
+            </div>
+
+            <button
+              className="bg-[#db1143f3] hover:bg-[#FF2E63] transition-colors text-white border-none outline-none w-[320px] md:w-[460px] 2xl:w-[520px] px-4 py-2 rounded cursor-pointer my-[1rem] text-base font-semibold"
+              type="submit"
+            >
+              Continue
+            </button>
+          </form>
+        </div>
+      </ContentWrapper>
     </div>
   );
 };
